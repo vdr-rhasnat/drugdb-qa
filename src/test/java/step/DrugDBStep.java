3b06c309ffd995ed54f9cf11002cb464180be9db
Cetus Rayhan Hasnat2 <vdr_rhasnat2@parata.com>
2021-09-15 09:40:41 +0000

    Swapped variables in assertEqual methods and renamed test_data directory name to drug_db_api_test_data

--- src/test/java/step/DrugDBStep.java
+++ src/test/java/step/DrugDBStep.java
@@ -11,6 +11,8 @@ public class DrugDBStep {
     private String token;
     private Integer statusCode;
     private String drugName;
+    private String color;
+    private String packageSize;
 
     @Given("A valid token with auth url {string}, client id {string}, client secret {string}, audience {string} and grand type {string}")
     public void aValidTokenWithAuthUrlClientIdClientSecretAudienceAndGrandType(String authUrl, String clientId, String clientSecret, String audience, String grandType) {
@@ -23,15 +25,27 @@ public class DrugDBStep {
         Response res = DrugDBRequest.GetByNdc(endpoint, token);
         statusCode = res.statusCode();
         drugName = res.then().extract().jsonPath().get("drugName");
+        color = res.then().extract().jsonPath().get("color");
+        packageSize = res.then().extract().jsonPath().get("packageSize");
     }
 
     @Then("I should receive status code {string}")
-    public void iShouldReceiveStatusCode(String status) {
-        Assert.assertEquals(statusCode.toString(), status);
+    public void iShouldReceiveStatusCode(String argStatus) {
+        Assert.assertEquals(argStatus, statusCode.toString());
     }
 
     @And("I should receive drug name {string}")
-    public void iShouldReceiveDrugName(String name) {
-        Assert.assertEquals(drugName, name);
+    public void iShouldReceiveDrugName(String argDrugName) {
+        Assert.assertEquals(argDrugName, drugName);
+    }
+
+    @And("I should receive color {string}")
+    public void iShouldReceiveColor(String argColor) {
+        Assert.assertEquals(argColor, color);
+    }
+
+    @And("I should receive package size {string}")
+    public void iShouldReceivePackageSize(String argPackageSize) {
+        Assert.assertEquals(packageSize, argPackageSize);
     }
 }
--- src/test/resources/feature/drugdb.feature
+++ src/test/resources/feature/drugdb.feature
@@ -1,7 +1,9 @@
 Feature: Test
-  @dataFile:testdata/test_data.xls
+  @dataFile:drug_db_api_test_data/test_cases.xls
   Scenario: "${test_case} - ${description}"
     Given A valid token with auth url "${auth_url}", client id "${client_id}", client secret "${client_secret}", audience "${audience}" and grand type "${grand_type}"
     When I hit the endpoint "${base_url}${extended_url}"
     Then I should receive status code "${res_status_code}"
     And I should receive drug name "${response_data}"
+    And I should receive color "${color}"
+    And I should receive package size "${package_size}"
